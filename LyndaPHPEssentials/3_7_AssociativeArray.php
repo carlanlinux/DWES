8cb004f9a1fcb74a208caaeaffbd2dd13a507a45
Carlos M. de Lucas <carlanlinux@gmail.com>
2019-11-26 21:36:15 +0100

    Correción ejemplos

--- LyndaPHPEssentials/3_7_AssociativeArray.php
+++ LyndaPHPEssentials/3_7_AssociativeArray.php
@@ -1,16 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Associative Array</title>
+    <title>Associative array</title>
 </head>
 <body>
+Associative Array: Se usa cuando no nos importa el orden en el Array.
+Associative Array
+An object-indexted array - Se guarda con etiquetas
+No tienen un orden y pueden ser refrenadas
+Se busca por etiquetas (the key) and the value
+Key-value pair —> Pareja llave valor
+Si importa el orden -> Array normal <hr>
 <?php
+//Creamos el Array como uno normal (Notación antigua)
+$assoc = array("first_name" => "Kevin", "last_name" => "Perez");
+//Se accede por la clave en vez de por índice, Indice no funciona.
+echo $assoc["first_name"];
+?> <br>
 
+<?php
+echo $assoc["first_name"] . " " . $assoc["last_name"];
+?> <br>
 
+<?php
+//Se modifica accediendo por clave
+$assoc["first_name"] = "Larry";
+echo $assoc["first_name"] . " " . $assoc["last_name"];
+?> <br>
+<?php
+//Ambos dos son lo mismo
+$numbers = [2, 4, 8, 16, 32];
+$numbers = [0 => 2, 1 => 4, 2 => 8, 3 => 16, 4 => 32];
+echo $numbers[0];
 ?>
+
 </body>
 </html>
-
-
-
